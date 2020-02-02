009ec165d07d41e8d61c4d8a12ae8d9452687175
jainshaurya23 <60533942+jainshaurya23@users.noreply.github.com>
2020-02-01 18:36:50 -0600

    Rolling and proximity

---
+++ Game project/Game with Us Game Jam 2020/Assets/Shaurya's Folder/Ball Roll/Assets/Rolling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling : MonoBehaviour
+{
+    public float speed = 2;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        float movementVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ Game project/Game with Us Game Jam 2020/Assets/Shaurya's Folder/Ball Roll/Assets/TextShow.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class TextShow : MonoBehaviour
+{
+
+	[Space(10)]
+	[Header("Toggle for the gui on off")]
+	public bool GuiOn = false;
+
+
+	[Space(10)]
+	[Header("The text to Display on Trigger")]
+	public string Text = "Turn Back";
+
+	[Tooltip("This is the window Box's size. It will be mid screen. Add or reduce the X and Y to move the box in Pixels. ")]
+	public Rect BoxSize = new Rect(0, 0, 200, 100);
+
+
+	[Space(10)]
+	public GUISkin customSkin;
+
+	void OnTriggerEnter()
+	{
+		GuiOn = true;
+	}
+
+
+	void OnTriggerExit()
+	{
+		GuiOn = false;
+	}
+
+	void OnGUI()
+	{
+
+		if (customSkin != null)
+		{
+			GUI.skin = customSkin;
+		}
+
+		if (GuiOn == true)
+		{
+
+			GUI.BeginGroup(new Rect((Screen.width - BoxSize.width) / 2, (Screen.height - BoxSize.height) / 2, BoxSize.width, BoxSize.height));
+			GUI.Label(BoxSize, Text);
+			GUI.EndGroup();
+
+		}
+
+
+	}
+
+}
